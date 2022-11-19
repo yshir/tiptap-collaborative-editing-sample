a8214be304a0a4fce5c441b1cf71bb3d9a7cd8d2
yshir <yshir0020@gmail.com>
2022-11-19 12:54:44 +0900

    feat: collaborative editing by WebRTC

--- src/Tiptap.tsx
+++ src/Tiptap.tsx
@@ -2,6 +2,43 @@ import './tiptap.scss';
 
 import { Editor, EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import Collaboration from '@tiptap/extension-collaboration';
+import CollaborationCursor from '@tiptap/extension-collaboration-cursor';
+import * as Y from 'yjs';
+import { WebrtcProvider } from 'y-webrtc';
+
+// A new Y document
+const ydoc = new Y.Doc();
+// Registered with a WebRTC provider
+const provider = new WebrtcProvider('example-document', ydoc);
+
+export default () => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        history: false,
+      }),
+      Collaboration.configure({
+        document: ydoc,
+      }),
+      CollaborationCursor.configure({
+        provider,
+        user: {
+          name: Date.now(),
+          color: '#13f3f3',
+        },
+      }),
+    ],
+    content: '',
+  });
+
+  return (
+    <div>
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  );
+};
 
 const MenuBar: React.FC<{ editor: Editor | null }> = ({ editor }) => {
   if (!editor) {
@@ -115,48 +152,5 @@ const MenuBar: React.FC<{ editor: Editor | null }> = ({ editor }) => {
       <button onClick={() => editor.chain().focus().undo().run()}>undo</button>
       <button onClick={() => editor.chain().focus().redo().run()}>redo</button>
     </>
-  );
-};
-
-export default () => {
-  const editor = useEditor({
-    extensions: [StarterKit],
-    content: `
-      <h2>
-        Hi there,
-      </h2>
-      <p>
-        this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-      </p>
-      <ul>
-        <li>
-          That’s a bullet list with one …
-        </li>
-        <li>
-          … or two list items.
-        </li>
-      </ul>
-      <p>
-        Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-      </p>
-      <pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-      <p>
-        I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-      </p>
-      <blockquote>
-        Wow, that’s amazing. Good work, boy! 👏
-        <br />
-        — Mom
-      </blockquote>
-    `,
-  });
-
-  return (
-    <div>
-      <MenuBar editor={editor} />
-      <EditorContent editor={editor} />
-    </div>
   );
 };
